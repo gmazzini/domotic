587f6ee562f6e009d482040cbd02d4968eafecc1
Gianluca Mazzini <gianluca@mazzini.org>
2017-01-08 09:44:14 +0100

    Update casa.php

--- casa.php
+++ casa.php
@@ -638,7 +638,7 @@ for(;;){
   // rele out on device 6
   for($j=50;$j<52;$j++){
     if($rele[$j]!=$rele_old[$j]){
-      $devstr=chr(52-$rele[$j]).chr($j+1).":0";
+      $devstr=chr(52-$rele[$j]).chr($j-1).":0";
       $mysock=socket_create(AF_INET,SOCK_DGRAM,SOL_UDP);
       socket_sendto($mysock,$devstr,strlen($devstr),0,"10.0.0.31",6723);
       socket_close($mysock);
