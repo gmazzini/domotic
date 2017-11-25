d62d98112f72dc45f6dde43e45d595684595c4f4
Gianluca Mazzini <gianluca@mazzini.org>
2017-11-25 11:59:08 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$config_version="54";
+$config_version="55";
 
 // 3level: 0 h_start h_end n_key key_1 ... key_n m_releA releA_1 ... releA_m q_releB releB_1 ... releB_q name
 // onoff: 1 h_start h_end n_key key_1 ... key_n m_rele rele_1 ... rele_m name
@@ -14,14 +14,11 @@ $config_version="54";
 // offtimed_keysup: 10 h_start h_end n_key key_1 ... key_n rele minutes name
 
 $act=array(
-array(6,5,20,4,12,"accendi acqua mattina"),
-array(7,5,40,4,12,"spegni acqua mattina"),
-array(6,22,20,4,12,"accendi acqua sera"),
-array(7,22,40,4,12,"spegni acqua sera"),
-array(1,0,23,1,4,1,12,"acqua"),
-array(8,0,23,1,14,1,19,"caldaia_automatica"),
-array(1,0,23,1,60,1,19,"caldaia"),
-array(10,0,23,1,14,19,60,"caldaia_autoff"),
+// array(6,5,20,4,12,"accendi acqua mattina"),
+// array(7,5,40,4,12,"spegni acqua mattina"),
+// array(6,22,20,4,12,"accendi acqua sera"),
+// array(7,22,40,4,12,"spegni acqua sera"),
+// array(1,0,23,1,4,1,12,"acqua"),
 array(1,0,23,3,30,6,16,7,43,37,9,12,16,26,13,"natale"),
 array(8,7,22,1,17,1,25,"campanello"),
 array(1,0,23,1,19,1,20,"cucina"),
