fe4a69e1b7b8ef336c09ef713757fa4435d11a3d
Gianluca Mazzini <gianluca@mazzini.org>
2020-11-16 15:29:16 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$config_version="80";
+$config_version="81";
 
 // 3level: 0 h_start h_end n_key key_1 ... key_n m_releA releA_1 ... releA_m q_releB releB_1 ... releB_q name
 // onoff: 1 h_start h_end n_key key_1 ... key_n m_rele rele_1 ... rele_m name
@@ -42,6 +42,7 @@ array(1,0,23,1,8,1,14,"barraTV"),
 array(1,0,23,1,23,2,13,11,"living piccola"),
 array(1,0,23,1,14,1,23,"ripostiglio"),
 array(1,0,23,1,37,1,0,"neve"),
+array(1,0,23,1,44,1,44,"neve_netto"),
 array(4,0,23,3,2,39,26,0,"spegnitutto"),
 array(3,0,23,1,5,22,0,1,24,25,27,28,31,32,33,34,35,36,39,40,41,42,44,45,46,47,56,57,"spegnisopra"),
 array(3,0,23,1,29,25,2,5,6,3,7,18,16,20,11,17,14,22,21,9,15,23,8,10,4,30,38,52,53,13,19,"spegnisotto"),
