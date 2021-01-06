68551be60c3da55f1583e5608582c8c7d3719b20
Gianluca Mazzini <gianluca@mazzini.org>
2021-01-06 20:05:40 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$config_version="101";
+$config_version="102";
 
 // 3level: 0 h_start h_end n_key key_1 ... key_n m_releA releA_1 ... releA_m q_releB releB_1 ... releB_q name
 // onoff: 1 h_start h_end n_key key_1 ... key_n m_rele rele_1 ... rele_m name
@@ -48,7 +48,7 @@ array(1,0,23,1,55,1,33,"neve_wood"),
 array(1,0,23,2,37,48,2,26,57,"sala_radio"),
 array(4,0,23,3,2,39,26,0,"spegnitutto"),
 array(3,0,23,2,5,52,19,0,24,26,27,28,31,32,33,34,35,36,39,40,41,42,44,45,46,47,"spegnisopra"),
-array(3,0,23,2,28,53,16,2,3,5,6,8,10,11,14,15,16,17,18,21,22,23,"spegnisotto"),
+array(3,0,23,2,28,53,15,2,3,5,6,8,10,11,14,15,16,17,18,21,23,"spegnisotto"),
 array(0,0,23,1,16,1,54,1,55,"cantina")
 );
 
