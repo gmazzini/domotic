62fa23b3fa34d46a46c55524717c94d3d6a86d93
Gianluca Mazzini <gianluca@mazzini.org>
2023-02-07 18:16:58 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$config_version="112";
+$config_version="113";
 
 // 0> 3level: 0 h_start h_end n_key key_1 ... key_n m_releA releA_1 ... releA_m q_releB releB_1 ... releB_q name
 // 1> onoff: 1 h_start h_end n_key key_1 ... key_n m_rele rele_1 ... rele_m name
@@ -22,7 +22,7 @@ $act=array(
 // array(6,22,20,4,12,"accendi acqua sera"),
 // array(7,22,40,4,12,"spegni acqua sera"),
 // array(1,0,23,1,4,1,12,"acqua"),
-array(1,0,23,4,12,18,30,49,11,1,12,13,19,20,21,22,37,43,48,56,"natale"),
+// array(1,0,23,3,12,18,30,11,1,12,13,19,20,21,22,37,43,48,56,"natale"),
 array(8,7,22,2,17,54,1,25,"campanello"),
 array(1,0,23,1,19,1,16,"cucina"),
 array(1,0,23,1,20,1,18,"barracucina"),
@@ -50,9 +50,11 @@ array(1,0,23,1,44,1,44,"neve_letto"),
 array(1,0,23,1,42,1,34,"neve_studio"),
 array(1,0,23,1,55,1,33,"neve_wood"),
 array(1,0,23,2,37,48,1,26,"sala_radio"),
-array(1,0,23,1,51,1,57,"sala_radio_scaffale"),
+array(1,0,23,1,49,1,61,"radio_left"),
+array(1,0,23,1,50,1,62,"radio_middle"),
+array(1,0,23,1,51,1,63,"radio_right"),  
 array(4,0,23,3,2,39,26,0,"spegnitutto"),
-array(3,0,23,2,5,52,20,0,24,26,27,28,31,32,33,34,35,36,39,40,41,42,44,45,46,47,57,"spegnisopra"),
+array(3,0,23,2,5,52,22,0,24,26,27,28,31,32,33,34,35,36,39,40,41,42,44,45,46,47,61,62,63,"spegnisopra"),
 array(3,0,23,2,29,53,14,2,3,5,6,8,10,11,14,15,16,17,18,21,23,"spegnisotto"),
 array(1,0,23,1,56,4,52,51,50,49,"atelier_luci"),
 array(1,0,23,1,57,1,55,"atelier_ap"),
