726840822d94c4942612fab601c984456d9389a9
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-17 22:04:10 +0200

    Add files via upload

---
+++ q3.php
@@ -0,0 +1,60 @@
+//releoutondevice1234
+for($dev=0;$dev<4;$dev++){
+  $q[0]=$q[1]=0;
+  $v[0]=$v[1]=0;
+  for($i=0;$i<12;$i++){
+    if($i<8)$gr=0;
+    else$gr=1;
+    $j=$i+$dev*12;
+    if($rele[$j]!=$rele_old[$j]){
+      fprintf($fplog,"out: %02d %01d %s\n",$j,$rele[$j],mytime_print($rele_time[$j]));
+      $q[$gr]++;
+    }
+    if($rele[$j])$v[$gr]+=$maskin[$i-4*$gr];
+    $rele_old[$j]=$rele[$j];
+  }
+  if($q[0]){
+    fwrite($fp[$dev],chr(0x43).chr($v[0]),2);
+    usleep($mysleep);
+  }
+  if($q[1]){
+    fwrite($fp[$dev],chr(0x46).chr($v[1]),2);
+    usleep($mysleep);
+  }
+}
+
+//releoutondevice5
+$mymsg1="";
+$myqr=0;
+for($j=48;$j<56;$j++){
+  if($rele[$j]!=$rele_old[$j]){
+    $mymsg1.="k0".chr($j+1)."=".chr(48+$rele[$j]).";";
+    fprintf($fplog,"out: %02d %01d %s\n",$j,$rele[$j],mytime_print($rele_time[$j]));
+    $rele_old[$j]=$rele[$j];
+    $myqr++;
+  }
+}
+if($myqr){
+  $myso1=socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
+  socket_connect($myso1,"10.0.0.32",5000);
+  socket_write($myso1,$mymsg1,strlen($mymsg1));
+  socket_close($myso1);
+}
+
+//releoutondevice7
+$mymsg1="";
+$myqr=0;
+for($j=56;$j<64;$j++){
+  if($rele[$j]!=$rele_old[$j]){
+    $mymsg1.="k0".chr($j-7)."=".chr(48+$rele[$j]).";";
+    fprintf($fplog,"out: %02d %01d %s\n",$j,$rele[$j],mytime_print($rele_time[$j]));
+    $rele_old[$j]=$rele[$j];
+    $myqr++;
+  }
+}
+if($myqr){
+  $myso1=socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
+  socket_connect($myso1,"10.0.0.34",5000);
+  socket_write($myso1,$mymsg1,strlen($mymsg1));
+  socket_close($myso1);
+}
