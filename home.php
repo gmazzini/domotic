621e8ad4f448f874f32ce8a9b81ba2349cf05b79
Gianluca Mazzini <gianluca@mazzini.org>
2024-05-17 21:47:03 +0200

    Update home.php

--- home.php
+++ home.php
@@ -1,83 +1,6 @@
 <?php
-
-// device 1 2 3 4 on 10.0.0.X with X=21 22 23 24 port 10001
-// IN 09 10 11 12 17 18 19 20 21 22 23 24
-// OUT 01 02 03 04 05 06 07 08 13 14 15 16
-// A(01-08) B(09-16) C(17-24)
-// A_Read=41H A_Conf=42H A_Write=43H
-// B_Read=44H B_Conf=45H B_Write=46H
-// C_Read=47H C_Conf=48H C_Write=4AH
-// Conf 1=Input 0=Output 
-// device 5 BEM106 out on 10.0.0.32 48-55 set x=R-47 http://10.0.0.32/k0x=1 on http://10.0.0.32/k0x=0 off
-// device 6 BEM108 in on 10.0.0.33 48-55 http://10.0.0.33/getpara[189]=1&getpara[190]=1&getpara[191]=1&getpara[192]=1&getpara[193]=1&getpara[194]=1&getpara[195]=1&getpara[196]=1
-// device 7 BEM106 out on 10.0.0.34 56-63 set x=R-55 http://10.0.0.34/k0x=1 on http://10.0.0.34/k0x=0 off
-// device 8 BEM108 in on 10.0.0.35 56-63 http://10.0.0.35/getpara[189]=1&getpara[190]=1&getpara[191]=1&getpara[192]=1&getpara[193]=1&getpara[194]=1&getpara[195]=1&getpara[196]=1
-
-// virtualkey 64-71
-
 $casa_version="67";
 $mydir="/home/gmazzini/casa/";
-
-// multiple output
-function multiout($port,$val){
-  global $fp,$mysleep;
-  for($dev=0;$dev<4;$dev++)fwrite($fp[$dev],chr($port).chr($val),2);
-  usleep($mysleep);
-}
-
-// multiple input
-function multiin($port){
-  global $fp,$mysleep;
-  multiout($port,0x00);
-  for($dev=0;$dev<4;$dev++)$myin[$dev]=ord(fread($fp[$dev],1));
-  return $myin;
-}
-
-// key check status
-function key_checkstatus($mykey){
-  global $inkey,$maskin,$keybasefordev,$keyassigneddev;
-  $dev=$keyassigneddev[$mykey];
-  if($inkey[$dev] & $maskin[$mykey-$keybasefordev[$dev]])return 0;
-  else return 1;
-}
-
-// rele set
-function myreleset($r,$val){
-  global $rele,$rele_time;
-  if($val)$rele[$r]=1;
-  else $rele[$r]=0;
-  $rele_time[$r]=mytime_up();
-}
-
-// system uptime with resolution of 10ms
-function mytime_up(){
-  list($usec,$sec)=explode(" ",microtime());
-  return $sec*100+(int)($usec*100);
-}
-
-// hours and minute
-function mytime_hhmm(){
-  $x2[0]=(int)date("H");
-  $x2[1]=(int)date("i");
-  return $x2;
-}
-
-// time string out
-function mytime_print($t){
-  global $mytime_ref;
-  $x1=$mytime_ref+(int)(((int)$t)/100);
-  $x2=((int)$t)-100*((int)(((int)$t)/100));
-  return date("d-m-Y/H:i:s",$x1).".".sprintf("%02d",$x2);
-}
-
-// load configuration
-function myconfig(){
-  global $nact,$passwd,$config_version,$act,$mydir;
-  include $mydir."config.php";
-  $nact=sizeof($act);
-}
-
-// initialization
 date_default_timezone_set("Europe/Rome");
 $mylog=$mydir."mylog";
 $fplog=fopen($mylog,"a+");
@@ -553,4 +476,62 @@ for(;;){
   }
 }
 
+// multiple output
+function multiout($port,$val){
+  global $fp,$mysleep;
+  for($dev=0;$dev<4;$dev++)fwrite($fp[$dev],chr($port).chr($val),2);
+  usleep($mysleep);
+}
+
+// multiple input
+function multiin($port){
+  global $fp,$mysleep;
+  multiout($port,0x00);
+  for($dev=0;$dev<4;$dev++)$myin[$dev]=ord(fread($fp[$dev],1));
+  return $myin;
+}
+
+// key check status
+function key_checkstatus($mykey){
+  global $inkey,$maskin,$keybasefordev,$keyassigneddev;
+  $dev=$keyassigneddev[$mykey];
+  if($inkey[$dev] & $maskin[$mykey-$keybasefordev[$dev]])return 0;
+  else return 1;
+}
+
+// rele set
+function myreleset($r,$val){
+  global $rele,$rele_time;
+  if($val)$rele[$r]=1;
+  else $rele[$r]=0;
+  $rele_time[$r]=mytime_up();
+}
+
+// system uptime with resolution of 10ms
+function mytime_up(){
+  list($usec,$sec)=explode(" ",microtime());
+  return $sec*100+(int)($usec*100);
+}
+
+// hours and minute
+function mytime_hhmm(){
+  $x2[0]=(int)date("H");
+  $x2[1]=(int)date("i");
+  return $x2;
+}
+
+// time string out
+function mytime_print($t){
+  global $mytime_ref;
+  $x1=$mytime_ref+(int)(((int)$t)/100);
+  $x2=((int)$t)-100*((int)(((int)$t)/100));
+  return date("d-m-Y/H:i:s",$x1).".".sprintf("%02d",$x2);
+}
+
+// load configuration
+function myconfig(){
+  global $nact,$passwd,$config_version,$act,$mydir;
+  include $mydir."config.php";
+  $nact=sizeof($act);
+}
 ?>
